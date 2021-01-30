cda4c948aa9823abdf1ec72b47af80312f40d455
JaridCoronado <74986316+JaridCoronado@users.noreply.github.com>
2021-01-30 16:28:15 -0500

    Project Created

---
+++ Coronado Facemelter Project 1/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public Text scoreCounter;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeScore();
+    }
+
+    public void AddToScore(int addScore)
+    {
+        score += addScore;
+    }
+
+    public void ChangeScore()
+    {
+        scoreCounter.text = score + "";
+    }
+}
